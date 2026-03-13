0aab1f9d8e145d715e716544c549d1a935698612
SinclairLin <lzx2155433@gmail.com>
2026-03-13 15:50:42 +0800

    alias vim to nvim when available

--- config/aliases.zsh
+++ config/aliases.zsh
@@ -6,9 +6,21 @@ alias ll='ls -lh'
 alias la='ls -lAh'
 # alias vim=nvim
 # alias vi=nvim
-# alias lg=lazygit
+if command -v nvim >/dev/null 2>&1; then
+  alias vim='nvim'
+  alias vi='nvim'
+fi
+alias lg=lazygit
 # alias rr=ranger
 
+# kitty ssh
+ssh() {
+  if [ "${TERM:-}" = "xterm-kitty" ] && command -v kitten >/dev/null 2>&1; then
+    kitten ssh "$@"
+  else
+    command ssh "$@"
+  fi
+}
 
 # git alias
 alias gco='git checkout'
@@ -19,7 +31,5 @@ alias gs='git --no-pager status'
 alias gss='git --no-pager status -s'
 alias gpt='git push origin --tags'
 alias glt='git tag -n --sort=taggerdate | tail -n ${1-10}'
-
-# alias waybar="bash ~/.script/waybar-restart.sh &"
 
 alias rp=randport
